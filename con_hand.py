072b58dd49a2cc73ce6804b9ffd96c15df965767
Davtechbenin1 <davtechbenin1@gmail.com>
2025-12-01 20:38:26 +0100

    Refactor con_hand.py with comments and structure
    Added French comments and improved code structure.

--- con_hand.py
+++ con_hand.py
@@ -349,6 +349,12 @@ class Data_handler:
 			return rows.get(ident, {})
 		return rows
 
+	def Multiple_get(self,base_name,tables_liste):
+		all_dic = dict()
+		for table in tables_liste:
+			all_dic[table] = self.Get_data(base_name,table)
+		return all_dic
+
 	def Save_data(self, base_name, table, data, data_ident):
 		"""
 		Sauvegarde ou met à jour une entrée dans la table.
@@ -429,5 +435,6 @@ class Data_handler:
 		self.Data_Table.setdefault(base_name, {})[table] = all_rows
 
 		return True
+
 
 
